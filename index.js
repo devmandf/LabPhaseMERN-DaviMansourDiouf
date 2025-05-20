cd91e81ba2ed2ae59889db067853d8e276f8535e
devinho <devmandf@gmail.com>
2025-05-20 19:10:07 +0000

    F3

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Définition du modèle Video
+const videoSchema = new mongoose.Schema({
+  name: String,
+  path: String,
+  active: Boolean
+});
+
+const Video = mongoose.model('Video', videoSchema);
+
 mongoose.connect("mongodb+srv://devinho:GeneraL123ForGlobalTest@cluster0.seme1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -13,6 +22,18 @@ app.use(express.static('public')); // Sert les fichiers statiques depuis le doss
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/client.html");
+});
+
+// Routes pour les vidéos
+app.get('/videos', async (req, res) => {
+  const videos = await Video.find();
+  res.json(videos);
+});
+
+app.patch('/videos/:id', async (req, res) => {
+  const { active } = req.body;
+  const video = await Video.findByIdAndUpdate(req.params.id, { active }, { new: true });
+  res.json(video);
 });
 
 app.listen(PORT, () => console.log("Server en ligne sur le port " + PORT));
